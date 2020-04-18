55b971122ac1a8cdde0abc90719823573af32bb0
Sagy Assor <sagyas12@gmail.com>
2020-04-18 21:26:17 +0300

    updated login function

--- app/src/main/java/com/example/betapp/AuthActivity.java
+++ app/src/main/java/com/example/betapp/AuthActivity.java
@@ -1,10 +1,12 @@
 package com.example.betapp;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.CheckBox;
@@ -12,18 +14,25 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.example.betapp.Services.AuthService.AuthService;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import android.view.inputmethod.InputMethodManager;
 
 public class AuthActivity extends AppCompatActivity {
     AuthService authService;
     CheckBox rememberMe;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
-        this.authService = AuthService.getInstance();
         rememberMe = findViewById(R.id.rememberMe);
+        mAuth = FirebaseAuth.getInstance();
 //        this.startService(); // TODO: uncomment to start notification service
 
         // check if 'remember me' checkbox was checks
@@ -53,9 +62,19 @@ public class AuthActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onStart(){
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        if (currentUser != null){
+//        openMyLeaguesActivity();
+//        }
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
-        this.authService.setIsLoggedIn(false);
+//        this.authService.setIsLoggedIn(false);
     }
 
     public void signIn(View view){
@@ -77,18 +96,7 @@ public class AuthActivity extends AppCompatActivity {
         String email_address = email_address_field.getText().toString();
         String password = password_field.getText().toString();
 
-        try {
-            authService.signInOrSignUp(email_address, password, AuthService.SIGN_IN);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if (authService.getIsLoggedIn()) {
-            openMyLeaguesActivity();
-
-        } else {
-            popupMessage("Credentials incorrect");
-        }
+        signIn(email_address, password);
     }
 
     public void signUp(View view){
@@ -148,6 +156,24 @@ public class AuthActivity extends AppCompatActivity {
     public void stopService(View v){
         Intent serviceIntent = new Intent(this, NotificationService.class);
         stopService(serviceIntent);
+    }
+
+    private void signIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            openMyLeaguesActivity();
+                        } else {
+                            popupMessage("Credentials incorrect");
+                        }
+
+                        // ...
+                    }
+                });
     }
 
 }
